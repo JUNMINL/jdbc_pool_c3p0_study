0eab2da95b8e28c81b59c12d2c36178169475b88
JUNMINL <say_l@naver.com>
2018-11-13 17:41:02 +0900

    Department Test

---
+++ src/test/java/jdbc_pool_c3p0_study/ConnectionProviderTest.java
@@ -0,0 +1,43 @@
+package jdbc_pool_c3p0_study;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import jdbc_pool_c3p0_study.jdbc.ConnectionProvider;
+import jdbc_pool_c3p0_study.jdbc.LogUtil;
+
+public class ConnectionProviderTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		System.out.println();
+		LogUtil.prnLog("Start ConnectionProviderTest");
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		System.out.println();
+		LogUtil.prnLog("End ConnectionProviderTest");
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		System.out.println();
+	}
+
+	@Test
+	public void test() {
+		try(Connection connection = ConnectionProvider.getConnection()){
+			LogUtil.prnLog(connection.toString());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
